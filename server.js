1117bb33d1e1725e02b1481dd90d0135e4f44f0e
lukaness <lucas.hoffmann.fitbit@gmail.com>
2026-01-09 16:47:40 -0500

    Update routes and file handling in server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ let eduUsers = {};
 
 // Define a simple route
 app.get('/', (req, res) => {
-    fs.readFile('./3Art.html', 'utf8', (err, data) => {
+    fs.readFile('./Home/Console_en.html', 'utf8', (err, data) => {
         if (err) {
             res.send(err);
         } else {
@@ -44,4 +44,69 @@ app.get('/objects', (req, res) => {
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get('/core', (req, res) => {
+    fs.readFile('./core/Home_core.html', 'utf8', (err, data) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(data);
+        }
+    })
+});
+
+app.get('/core/home', (req, res) => {
+    fs.readFile('./core/Home_core.html', 'utf8', (err, data) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(data);
+        }
+    })
+});
+
+app.get('/edu', (req, res) => {
+    fs.readFile('./edu/Home_edu.html', 'utf8', (err, data) => {
+        if (err) {
+            res.send(err);
+            console.error("Error:" + err);
+        } else {
+            res.send(data);
+        }
+    })
+});
+
+app.post('/save-user', (req, res) => {
+    const userInfo = req.body; // Get the object from the request body
+    fs.writeFile('./edu/users/Users.json', 'utf8', (err, data) => {});
+    fs.appendFile('./edu/users/Users.json', '{' + userInfo + '}', (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('Content has been appended');
+    });
+});
+
+app.get('/users', (req, res) => {
+fs.readFile('./edu/users/Users.json', 'utf8', (err, data) => {
+    if (err) {
+    res.send(err);
+    } else {
+    res.json(data);
+    }
+
+    });
+});
+
+app.get('/edu/openplay', (req, res) => {
+    fs.readFile('./edu/Play_edu.html', 'utf8', (err, data) => {
+        if (err) {
+            res.send(err);
+            console.error("Error:" + err);
+        } else {
+            res.send(data);
+        }
+    })
 });
